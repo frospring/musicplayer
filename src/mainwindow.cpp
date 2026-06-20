76ee142192e034ad299920d59243af3c9d6bc016
asai <1663802680@qq.com>
2026-06-20 22:19:12 +0800

    音乐加载与播放功能

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -8,9 +8,12 @@
 #include <QHBoxLayout>
 #include <QMediaPlayer>
 #include <QAudioOutput>
+#include <QFileDialog>
+#include <QUrl>
+#include <QFileInfo>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+    : QMainWindow(parent), m_isPlaying(false)
 {
     setWindowTitle("音乐播放器");
     resize(500, 400);
@@ -28,8 +31,10 @@ MainWindow::MainWindow(QWidget *parent)
     m_lyricsLabel->setAlignment(Qt::AlignCenter);
 
     m_playBtn = new QPushButton("播放", this);
+    m_openBtn = new QPushButton("打开文件", this);
 
     m_progressSlider = new QSlider(Qt::Horizontal, this);
+    m_progressSlider->setRange(0, 0);
 
     m_playlist = new QListWidget(this);
 
@@ -38,6 +43,7 @@ MainWindow::MainWindow(QWidget *parent)
     infoLayout->addWidget(m_artistLabel);
 
     auto *btnLayout = new QHBoxLayout;
+    btnLayout->addWidget(m_openBtn);
     btnLayout->addWidget(m_playBtn);
 
     layout->addLayout(infoLayout);
@@ -49,4 +55,80 @@ MainWindow::MainWindow(QWidget *parent)
     m_player = new QMediaPlayer(this);
     m_audioOutput = new QAudioOutput(this);
     m_player->setAudioOutput(m_audioOutput);
+
+    connect(m_openBtn, &QPushButton::clicked, this, &MainWindow::onOpenFile);
+    connect(m_playBtn, &QPushButton::clicked, this, &MainWindow::onPlayPause);
+    connect(m_player, &QMediaPlayer::playbackStateChanged, this, &MainWindow::onPlayerStateChanged);
+    connect(m_player, &QMediaPlayer::positionChanged, this, &MainWindow::onPositionChanged);
+    connect(m_player, &QMediaPlayer::durationChanged, this, &MainWindow::onDurationChanged);
+    connect(m_progressSlider, &QSlider::sliderMoved, this, &MainWindow::onSliderMoved);
+    connect(m_playlist, &QListWidget::itemDoubleClicked, this, &MainWindow::onItemDoubleClicked);
+}
+
+void MainWindow::onOpenFile()
+{
+    QStringList files = QFileDialog::getOpenFileNames(
+        this, "选择音乐文件", QString(),
+        "音频文件 (*.mp3 *.wav *.flac *.ogg *.m4a)");
+
+    for (const auto &path : files) {
+        m_playlist->addItem(path);
+    }
+}
+
+void MainWindow::onPlayPause()
+{
+    if (m_playlist->currentRow() < 0 && m_playlist->count() > 0) {
+        m_playlist->setCurrentRow(0);
+        onItemDoubleClicked(m_playlist->item(0));
+        return;
+    }
+
+    if (m_isPlaying) {
+        m_player->pause();
+    } else {
+        m_player->play();
+    }
+}
+
+void MainWindow::onPlayerStateChanged()
+{
+    auto state = m_player->playbackState();
+    if (state == QMediaPlayer::PlayingState) {
+        m_isPlaying = true;
+        m_playBtn->setText("暂停");
+    } else {
+        m_isPlaying = false;
+        m_playBtn->setText("播放");
+    }
+}
+
+void MainWindow::onPositionChanged(qint64 pos)
+{
+    if (!m_progressSlider->isSliderDown()) {
+        m_progressSlider->setValue(static_cast<int>(pos));
+    }
+}
+
+void MainWindow::onDurationChanged(qint64 dur)
+{
+    m_progressSlider->setRange(0, static_cast<int>(dur));
+}
+
+void MainWindow::onSliderMoved(int val)
+{
+    m_player->setPosition(val);
+}
+
+void MainWindow::onItemDoubleClicked(QListWidgetItem *item)
+{
+    if (!item) return;
+
+    QString path = item->text();
+    m_player->setSource(QUrl::fromLocalFile(path));
+
+    QFileInfo fi(path);
+    m_titleLabel->setText(fi.completeBaseName());
+
+    m_player->play();
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -7,6 +7,7 @@ class QLabel;
 class QPushButton;
 class QSlider;
 class QListWidget;
+class QListWidgetItem;
 class QMediaPlayer;
 class QAudioOutput;
 
@@ -18,15 +19,26 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow() = default;
 
+private slots:
+    void onOpenFile();
+    void onPlayPause();
+    void onPlayerStateChanged();
+    void onPositionChanged(qint64 pos);
+    void onDurationChanged(qint64 dur);
+    void onSliderMoved(int val);
+    void onItemDoubleClicked(QListWidgetItem *item);
+
 private:
     QLabel *m_titleLabel;
     QLabel *m_artistLabel;
     QLabel *m_lyricsLabel;
     QPushButton *m_playBtn;
+    QPushButton *m_openBtn;
     QSlider *m_progressSlider;
     QListWidget *m_playlist;
     QMediaPlayer *m_player;
     QAudioOutput *m_audioOutput;
+    bool m_isPlaying;
 };
 
 #endif
