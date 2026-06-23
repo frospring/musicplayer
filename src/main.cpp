b405d1b186166e47951f8e252688c3604b5e9b3f
asai <1663802680@qq.com>
2026-06-23 09:05:04 +0800

    main.cpp: QApplication改为QGuiApplication, QQmlApplicationEngine加载QML界面

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
-#include <QApplication>
-#include "mainwindow.h"
+// zkw2024051604060
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    QGuiApplication app(argc, argv);
 
-    MainWindow w;
-    w.show();
+    QQmlApplicationEngine engine;
+    engine.load(QUrl::fromLocalFile("qml/Main.qml"));
+
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
     return app.exec();
 }
